5a25a753a5d7919352b0d9f9c28a49e12b8b8f5c
Adrian Mtz <68370468+AdrianMtz-Santana@users.noreply.github.com>
2021-05-06 15:04:13 -0500

    Update 1_QIIME2_IMPORTAR.sh

--- 16S_R_scripts/1_QIIME2_IMPORTAR.sh
+++ 16S_R_scripts/1_QIIME2_IMPORTAR.sh
@@ -7,7 +7,13 @@ source /share/apps/External/Miniconda3-4.7.10/bin/activate
 
 export PATH=/share/apps/External/Miniconda3-4.6.14/envs/qiime2-2019.10/bin:$PATH
 conda activate qiime2-2019.10
-
+#Para single end 
+#qiime tools import \
+  #--type 'SampleData[SequencesWithQuality]' \
+ # --input-path manifesto.csv \
+ # --output-path demux.qza \
+#  --input-format SingleEndFastqManifestPhred33
+#para Paired-end
 qiime tools import \
   --type SampleData[PairedEndSequencesWithQuality] \
   --input-format PairedEndFastqManifestPhred33 \
